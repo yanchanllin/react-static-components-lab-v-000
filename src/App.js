ac4771585471a0f45e1acd1f430b63c71397d37c
Yan C Lin <yonalin1@gmail.com>
2019-05-03 17:02:32 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ class App extends Component {
 		// your code in the return statement below!
 		return (
 			<div className="App">
+
+			<CatComponent / >
+			<EinsteinQuoteComponent / >
+			<MouseComponent / >
 			</div>
 		);
 	}
